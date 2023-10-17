b77edb1d76f8e0241a7fa37be468d471acccc7ff
Will Chen <wfchen1990@gmail.com>
2023-10-17 16:27:58 -0600

    Add merchant show response test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,4 +10,14 @@ describe "Merchants API" do
 
     merchants = JSON.parse(response.body, symbolize_names: true)
   end
+
+  it 'sends a single merchant' do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/#{merchant.id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+  end
 end
